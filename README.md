7c40584ca140ea8366888045f607eeb5f46cede3
GenAnderson <genchung1@outlook.com>
2023-04-11 13:24:15 -1000

    added missing comma in pokemonList and added some notes in readme file

---
+++ README.md
@@ -0,0 +1,4 @@
+Project Description:
+How to get the project running:
+Project dependencies:
+Which API the project uses:
--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,17 @@
 let pokemonList = [
   {
-  name: 'Gengar',
-  height: 1.5,
-  types: ['ghost', 'poison']
+    name: "Gengar",
+    height: 1.5,
+    types: ["ghost", "poison"],
   },
   {
-  name: 'Ditto',
-  height: 0.3,
-  types: ['normal']
-}
-{
-  name: 'Psyduck',
-  height: 0.8,
-  types: ['water']
-}
+    name: "Ditto",
+    height: 0.3,
+    types: ["normal"],
+  },
+  {
+    name: "Psyduck",
+    height: 0.8,
+    types: ["water"],
+  },
 ];
