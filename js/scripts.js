af9cc91362b092fdc4f167c70fac9d81c988d690
GenAnderson <genchung1@outlook.com>
2023-04-10 16:33:56 -1000

    added array in script.js
    added the pokemonList array

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,17 @@
-alert("Hello world");
-
-let favoriteFood = "mango";
+let pokemonList = [
+  {
+  name: 'Gengar',
+  height: 1.5,
+  types: ['ghost', 'poison']
+  },
+  {
+  name: 'Ditto',
+  height: 0.3,
+  types: ['normal']
+}
+{
+  name: 'Psyduck',
+  height: 0.8,
+  types: ['water']
+}
+];
