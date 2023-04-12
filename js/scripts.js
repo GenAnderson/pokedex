fd3ca324c933807b90d31f5fb7fe17f96a6bf10a
GenAnderson <genchung1@outlook.com>
2023-04-11 16:11:10 -1000

    initial commit didnt work. changes made to js file includes for loop and function

--- js/scripts.js
+++ js/scripts.js
@@ -15,3 +15,22 @@ let pokemonList = [
     types: ["water"],
   },
 ];
+
+for (i = 0; i < pokemonList.length; i++) {
+  document.write(
+    `<p> ${pokemonList[i].name} (height: ${pokemonList[i].height}) ${
+      pokemonList[i].height > 1 ? " - Wow, thats big!" : " "
+    }</p>`
+  );
+}
+
+function divide(dividend, divisor) {
+  if (divisor === 0) {
+    return "You're trying to divide by zero";
+  } else {
+    let result = dividend / divisor;
+    return result;
+  }
+}
+
+console.log(divide(1, 4));
