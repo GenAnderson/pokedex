e6100bbf66abaaf3318c84fdd8ba8d04d8848fef
GenAnderson <genchung1@outlook.com>
2023-04-19 13:18:30 -1000

    fixed modal styling in css. added function for card colors in js

--- js/scripts.js
+++ js/scripts.js
@@ -37,7 +37,9 @@ let pokemonRepository = (function () {
         item.name = details.name;
         item.imageUrl = details.sprites.front_default;
         item.height = details.height;
+        item.weight = details.weight;
         item.types = details.types;
+        item.abilities = details.abilities;
       })
       .catch(function (e) {
         console.error(e);
@@ -48,19 +50,70 @@ let pokemonRepository = (function () {
   function displayModal(item) {
     let titleElement = document.querySelector(".modal-title");
     let heightElement = document.querySelector(".modal-body--height");
+    let weightElement = document.querySelector(".modal-body--weight");
+    let typesElement = document.querySelector(".modal-body--types");
+    let abilitiesElement = document.querySelector(".modal-body--abilities");
     let pokemonImage = document.querySelector(".modal-img");
 
     // Empty previous modal content
     titleElement.innerText = "";
     heightElement.innerText = "";
+    weightElement.innerText = "";
     pokemonImage.src = "";
+    typesElement.innerText = "";
+    abilitiesElement.innerText = "";
 
     pokemonRepository.loadDetails(item).then(function () {
       // Insert new modal content
+      let typesElementArr = "";
+      let abilitiesElementArr = "";
+
+      item.types.forEach((type) => {
+        typesElementArr += [`<li> ${type.type.name} </li>`];
+      });
+
+      item.abilities.forEach((ability) => {
+        abilitiesElementArr += [`<li> ${ability.ability.name} </li>`];
+      });
+
       titleElement.innerText = `#${item.id} ${item.name}`;
-      heightElement.innerText = `Height: ${item.height / 10} m`;
+      heightElement.innerText = `Height:  ${item.height / 10} m`;
+      weightElement.innerText = `Weight:  ${item.weight / 10} lbs`;
       pokemonImage.src = item.imageUrl;
+      typesElement.innerHTML = typesElementArr;
+      abilitiesElement.innerHTML = abilitiesElementArr;
+
+      cardColor(item);
     });
+  }
+
+  // My own side function for pokemon card colors
+  function cardColor(item) {
+    let color = [];
+    let typesArr = [];
+    let pokemonCard = document.querySelector(".modal-content");
+
+    pokemonCard.style.background = "";
+    item.types.forEach((type) => {
+      typesArr.push(type.type.name);
+    });
+
+    const typeColor = {
+      water: "linear-gradient(to top, rgb(125, 157, 243), white)",
+      fire: "linear-gradient(to top, rgb(207, 60, 34), white)",
+      ground: "linear-gradient(to top, rgb(136, 115, 71), white)",
+      grass: "linear-gradient(to top, rgb(81, 240, 81), white)",
+      electric: "linear-gradient(to top, rgb(248, 238, 97), white)",
+    };
+
+    typesArr.find((item) => {
+      Object.entries(typeColor).find(function (x) {
+        if (item === x[0]) {
+          color = x;
+        }
+      });
+    });
+    pokemonCard.style.background = color[1];
   }
 
   // adds a pokemon to pokelist
